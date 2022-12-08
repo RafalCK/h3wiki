00558008c13bb26a80bb03c6827a145f92fdc302
Rafal Nowak <rafalnowak62@o2.pl>
2022-12-08 17:36:11 +0100

    Finished home page

--- src/App.vue
+++ src/App.vue
@@ -8,22 +8,4 @@
 </script>
 
 <style lang="scss">
-	// body {
-	// display: flex;
-    // flex-direction: column;
-    // color: var(--main-text);
-    // font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
-    // font-size: 1rem;
-	// }
-
-	
-	// .body__bg {
-	// background-image: url(./assets/images/background.jpg);
-    // background-size: cover;
-    // height: auto;
-    // background-attachment: fixed;
-    // background-position: center top;
-    // background-repeat: no-repeat;
-    // background-color: black
-	// }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,22 +10,22 @@
 			<div class="home__subheader">
 				<span class="subheader__inscription">wiki</span>
 			</div>
+			<div class="home__search">
+				<button class="search__button" @click="openSearch()">
+				<span class="search__text">Szukaj</span>
+				</button>
+			</div>
 		</div>
 		<div class="home__right">
-			<div class="menu">
-			<ul>
-				<li>Jednostki</li>
-				<li>Lokacje</li>
-			</ul>
-		</div>
+			<main-menu/>
 		</div>
 	</div>
 </div>
 </template>
 
-<setup script>
-
-</setup>
+<script setup>
+import MainMenu from '../components/menu/MainMenu.vue';
+</script>
 
 
 <style lang="scss" scoped>
@@ -59,6 +59,10 @@
 
 	&__right {
 		flex: 1 1 0;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100%;
 	}
 
 	&__header {
@@ -71,6 +75,18 @@
 		color: $color-white;
 		text-transform: uppercase;
 		margin-top: rem(-85);
+	}
+
+	&__search {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+
+		margin-top: rem(35);
+
+		&:hover {
+    		filter: drop-shadow(0px 0px 25px rgba(244, 247, 249, 1));
+		}
 	}
 
 	.header {
@@ -95,6 +111,78 @@
 		}
 	}
 
+	.search {
+
+	&__button {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+
+		height: 100%;
+
+		background: transparent;
+		border: 0;
+		background-image: url('src/assets/images/menu/vision.png');
+		height: rem(100);
+		background-size: rem(110);
+		background-position: top left;
+		background-repeat: no-repeat;
+	}
+
+	&__text {
+		background-image: repeating-linear-gradient(#eee 0px, #ebc622 15px, #eee 30px);
+		font-size: rem(30);
+		font-weight: 700;
+		letter-spacing: 0.5px;
+		text-transform: uppercase;
+		background-clip: text;
+		-webkit-background-clip: text;
+		color: transparent;
+		-webkit-text-fill-color: transparent;
+		filter: drop-shadow(0 1px 3px black) drop-shadow(0 1px 2px black) drop-shadow(0 1px 1px black);
+		cursor: pointer;
+	}
+	}
+
+}
+
+@media (max-width: 840px) {
+	.home {
+
+		height: 100%;
+
+		&__background {
+			width: 100%;
+			height: 100%;
+			flex-direction: column;
+		}
+
+		&__right {
+			justify-content: center;
+		}
+
+		.header {
+			&__inscription {
+				font-size: rem(45);
+				vertical-align: top;
+				&-letter {
+					font-size: rem(100);
+					line-height: rem(175);
+				}
+				&-number {
+					font-size: rem(100);
+					letter-spacing: rem(-15);
+					line-height: rem(175);
+				}
+			}
+		}
+
+		.subheader {
+			&__inscription {
+				font-size: rem(20);
+			}
+	}
+	}
 }
 
 </style>
